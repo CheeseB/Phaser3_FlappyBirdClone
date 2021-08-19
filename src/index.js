1d9b4967b6deee503c8552d084092e78bf8d0c2f
CheeseB <2489ckckck@naver.com>
2021-08-20 00:04:58 +0900

    add sprite, move back & forward

--- src/index.js
+++ src/index.js
@@ -6,10 +6,14 @@ const config = {
   height: 600,
   physics: {
     default: "arcade",
+    arcade: {
+      debug: true,
+    },
   },
   scene: {
     preload,
     create,
+    update,
   },
 };
 
@@ -17,8 +21,24 @@ new Phaser.Game(config);
 
 function preload() {
   this.load.image("sky-bg", "assets/sky.png");
+  this.load.image("bird", "assets/bird.png");
 }
+
+const VELOCITY = 200;
+let bird = null;
 
 function create() {
   this.add.image(0, 0, "sky-bg").setOrigin(0);
+  bird = this.physics.add
+    .sprite(config.width / 10, config.height / 2, "bird")
+    .setOrigin(0);
+  bird.body.velocity.x = VELOCITY;
+}
+
+function update() {
+  if (bird.x >= config.width - bird.width) {
+    bird.body.velocity.x = -VELOCITY;
+  } else if (bird.x <= 0) {
+    bird.body.velocity.x = VELOCITY;
+  }
 }
