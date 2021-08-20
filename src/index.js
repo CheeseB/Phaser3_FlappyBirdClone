7144160288891cce6e5d34af8e0a96af2ec9bb31
CheeseB <2489ckckck@naver.com>
2021-08-20 11:34:09 +0900

    add restart function

--- src/index.js
+++ src/index.js
@@ -28,20 +28,31 @@ function preload() {
 }
 
 let bird = null;
-let flapVelocity = 250;
+const flapVelocity = 250;
+const initialBirdPosition = { x: config.width * 0.1, y: config.height / 2 };
 
 function create() {
   this.add.image(0, 0, "sky-bg").setOrigin(0);
   bird = this.physics.add
-    .sprite(config.width / 10, config.height / 2, "bird")
+    .sprite(initialBirdPosition.x, initialBirdPosition.y, "bird")
     .setOrigin(0);
 
   this.input.on("pointerdown", flap);
   this.input.keyboard.on("keydown_SPACE", flap);
 }
 
-function update() {}
+function update() {
+  if (bird.y < -bird.height || bird.y > config.height) {
+    restartBirtPosition();
+  }
+}
 
 function flap() {
   bird.body.velocity.y = -flapVelocity;
+}
+
+function restartBirtPosition() {
+  bird.x = initialBirdPosition.x;
+  bird.y = initialBirdPosition.y;
+  bird.body.velocity.y = 0;
 }
