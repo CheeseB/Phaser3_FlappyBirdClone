57f5309276831b7716e1831be6e7f48a3093b4c8
CheeseB <2489ckckck@naver.com>
2021-08-24 00:29:08 +0900

    add menu item

--- src/scenes/BaseScene.js
+++ src/scenes/BaseScene.js
@@ -4,10 +4,23 @@ class BaseScene extends Phaser.Scene {
   constructor(key, config) {
     super(key);
     this.config = config;
+    this.screenCenter = [config.width / 2, config.height / 2];
   }
 
   create() {
     this.add.image(0, 0, 'sky-bg').setOrigin(0);
+  }
+
+  createMenu(menu) {
+    let lastMenuPositionY = 0;
+
+    menu.forEach((menuItem) => {
+      const menuPosition = [this.screenCenter[0], this.screenCenter[1] + lastMenuPositionY];
+      this.add
+        .text(...menuPosition, menuItem.text, { fontSize: '32px', fill: '#000' })
+        .setOrigin(0.5, 1);
+      lastMenuPositionY += 42;
+    });
   }
 }
 
