b04d1db6b63cb359dd3f6d0c69f2b524e7fb4059
CheeseB <2489ckckck@naver.com>
2021-08-24 16:37:13 +0900

    add difficulty

--- src/scenes/PlayScene.js
+++ src/scenes/PlayScene.js
@@ -11,15 +11,30 @@ class PlayScene extends BaseScene {
     this.pipes = null;
     this.isPaused = false;
 
-    this.pipeVerticalDistanceRange = [150, 250];
-    this.pipeHorizontalDistanceRange = [300, 400];
-    this.flapVelocity = 250;
+    this.flapVelocity = 300;
 
     this.score = 0;
     this.scoreText = '';
+
+    this.currentDifficulty = 'easy';
+    this.defficulties = {
+      easy: {
+        pipeHorizontalDistanceRange: [300, 500],
+        pipeVerticalDistanceRange: [150, 200],
+      },
+      normal: {
+        pipeHorizontalDistanceRange: [280, 330],
+        pipeVerticalDistanceRange: [140, 190],
+      },
+      hard: {
+        pipeHorizontalDistanceRange: [250, 310],
+        pipeVerticalDistanceRange: [120, 170],
+      },
+    };
   }
 
   create() {
+    this.currentDifficulty = 'easy';
     super.create();
     this.createBird();
     this.createPipes();
@@ -158,9 +173,10 @@ class PlayScene extends BaseScene {
   }
 
   placePipe(uPipe, lPipe) {
-    const pipeVerticalDistance = Phaser.Math.Between(...this.pipeVerticalDistanceRange);
+    const difficulty = this.defficulties[this.currentDifficulty];
+    const pipeVerticalDistance = Phaser.Math.Between(...difficulty.pipeVerticalDistanceRange);
     const pipeVerticalPosition = Phaser.Math.Between(0, this.config.height - pipeVerticalDistance);
-    const pipeHorizontalDistance = Phaser.Math.Between(...this.pipeHorizontalDistanceRange);
+    const pipeHorizontalDistance = Phaser.Math.Between(...difficulty.pipeHorizontalDistanceRange);
     const rightMostX = this.getRightMostPipe();
 
     uPipe.x = rightMostX + pipeHorizontalDistance;
